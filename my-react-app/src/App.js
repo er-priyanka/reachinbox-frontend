c51d106d178c5d64e1bac68df28690491341dbeb
er-priyanka <shahanepriyankas01@gmail.com>
2024-03-23 16:25:16 +0530

    worked on login page. UI part done

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import Login from './Pages/Login';
+import {Login} from './Pages/Login';
+import { Box, Button, useColorMode } from "@chakra-ui/react";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+
+ 
   return (
     <div className="App">
-     WELCOME TO FRONTEND
+     {/* <Box p="10px 40px" border="1px" textAlign={"right"} >
+        <Button 
+        border={"1px"}
+        onClick={toggleColorMode}
+        >Toggle {colorMode == 'light' ? 'Dark' : 'Light'} </Button>
+
+      </Box> */}
      <Login />
     </div>
   );
--- my-react-app/src/Pages/Login.jsx
+++ my-react-app/src/Pages/Login.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from 'react';
+import { Box, Image, Stack, Text, Button, Link, Center } from "@chakra-ui/react";
+// import axios from "axios";
+// import { authContext } from '../Context/AuthContext';
+import logoImg from "../Images/logo.png";
+import { FcGoogle } from "react-icons/fc";
 
-function Login() {
+// const signupUser = async() =>{
+//   const res = await axios.get("https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://frontend.com");
+//   console.log(res);
+//   return res;
+// }
+
+
+export const Login = () => {
+  // const {isAuth, setIsAuth} = useContext(authContext);
+
+  
   return (
-    <div>login</div>
+    <Box  fontFamily={"Open Sans"}>
+       
+      <Center 
+        p="10px 100px"
+        w={"100%"}
+        h={"64px"}
+        borderBottom={"1px"}
+        borderColor={"#25262B"}
+        pos={"fixed"}
+        top={0}
+      >
+          <Box>
+            <Image src={logoImg} alt="ReachInBox logo" />
+          </Box>
+          
+      </Center>
+       
+
+      <Center h="100vh">
+        <Stack 
+        w={"25%"} 
+        m={"auto"}
+        p={"14px 40px 40px 40px"}
+        spacing={"48px"}
+        border="1px"
+        borderRadius={"17px"}
+        borderColor={"#343A40"}
+       
+        >
+          <Stack spacing={"24px"}>
+            <Text size={"20px"}>Create a new account</Text>
+            <Button 
+            border={"1px solid #707172"}
+            display={"Flex"}
+            gap={"10px"}
+            p="8px 16px"
+            borderRadius={"4px"}
+            ><FcGoogle /> Sign Up with Google</Button>
+          </Stack>
+
+          <Stack
+          spacing={"24px"}
+          >
+            <Button 
+            bgGradient="linear(to-r, #4B63DD, #0524BF)"
+            borderRadius="4px"
+            padding="13px 35px"
+            fontSize={"14px"}
+            _hover={{
+              backgroundColor:"#0524BF"
+            } 
+            }
+            >Create an Account</Button>
+              <Text 
+              fontSize={"14px"}
+              color={"#909296"}
+              fontFamily={"Open Sans"}
+              fontWeight={"400"}
+              >
+                Already have an account? <Link color={"white"} href='/signin'>Sign In</Link>
+              </Text>
+          </Stack>
+        </Stack>
+      </Center>
+      
+    </Box>
+    
   )
 }
 
-export default Login;
